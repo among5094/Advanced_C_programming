174d7aa03c398c150b50c2f74e254661381938ad
among5094 <among5094@gmail.com>
2022-12-09 21:49:36 +0900

    이중포인터 -2번

--- ch14_doublePointer_Training/doublePointer_2.c
+++ ch14_doublePointer_Training/doublePointer_2.c
@@ -1,30 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 // 14장 - 이중포인터  2번: 
+//https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=kbc91&logNo=220623922237
 
-int get_sum(int array);
+int get_sum(int array[], int size);
 
 int main(void)
 {
-    int arr[3][5]={
-        {10,20,30,40,50},
-        {1,2,3,4,5},
-        {11,22,33,44,55}
-    };
+    //이차원배열에 덧셈 저장하기
+    int arr[3][5] = {{10,20,30,40,50},
+                     {1,2,3,4,5},
+                     {11,22,33,44,55}};
 
+    int (*pst)[5]=arr; // 배열 포인터 선언 시 [] <- 들어가는 값은 2차원 배열의 열 크기와 동일해야함
+    
+    // 한 행의 합 먼저 구하기
     int sum=0;
-    
-    for(int col=0 ; col<3 ; col++) //행증가
-    {    
-        for(int row=0; row<5; row++)//열증가
-        {
-            sum=sum+arr[col][row];
-        } 
+    for(int k=0;k<3;k++)
+    {
+        for(int i=0; i<5;i++) // 여기서 i는 2차원 배열의 열 인덱스
+            sum = sum + *(*(pst+k)+i); // *(pst) --> 행, *((행)+i) --> 열
     }
-    
     printf("합계: %d\n", sum);
-
+    
     return 0;
 }
-
-int get_sum(int array);
---
+++ ch14_doublePointer_Training/doublePointer_3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+
+int main(void)
+{
+   
+    double avg[3]; //Æò±Õµµ 3°³
+
+    return 0;
+}
