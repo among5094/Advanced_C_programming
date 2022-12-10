198744d59d0b61903a497e033766f81bf0cd6dd9
among5094 <among5094@gmail.com>
2022-12-10 22:39:08 +0900

    최종version

--- ch14_doublePointer_Training/doublePointer_4-2.c
+++ ch14_doublePointer_Training/doublePointer_4-2.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+/*
+문제 속에 답이 있다. 문제: 문자열의 "배열을 인수로 받아서" 저장된 문자열을 전부 출력하는 함수를 작성하여 테스트하자.
+이중포인터를 받으려면 포인터가 선언되어야한다.
 
-//저장된 문자열을 전부 출력하는 함수 선언
++)참고: call by value, call by address에 대한 설명 블로그
+https://ansan-survivor.tistory.com/1255
+교재p. 614~615
+*/
+
 void pr_str_array(char **dp, int n);
 
 int main(void)
@@ -19,16 +26,17 @@ int main(void)
     };
 
     pr_str_array(arr, 10);//함수 호출
-
     return 0;
 }
 
 void pr_str_array(char **dp, int n)
 {
-    //출력부분
     for (int i=0;i<n;i++)
-        printf("%s\n", dp[i]);
-
+        printf("%s\n", dp[i]); //이 부분 설명 더 업데이트하기
+        /*
+        왜 그냥 dp[i]이것만 쓸까? 앞에 *는 필요없는걸까
+        -> 문자열이기 때문에 안 써주는 것이고, 문자면 써주어야 한다.
+        */
  }
 
  
