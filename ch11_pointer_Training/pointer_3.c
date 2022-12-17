7b44f42212772ee0306bc1301921ec1dfc65f103
among5094 <among5094@gmail.com>
2022-12-18 03:01:39 +0900

    ch11 포인터 3번문제 재풀이

--- ch11_pointer_Training/pointer_3.c
+++ ch11_pointer_Training/pointer_3.c
@@ -7,7 +7,7 @@ void arr_fill(int* A, int size);
 
 int main(void)
 {
-    int arr[20]; //여기에 20을 미리 쓰면 동적 할당이 아니지 않나? 동적 할당이 아니라도 상관없는 건가?
+    int arr[20];
     arr_fill(arr,20);
     
     for(int i=0;i<20;i++)
@@ -18,9 +18,6 @@ int main(void)
 
 void arr_fill(int* A, int size)
 {
-
-    srand(time(NULL));
     for(int i=0;i<size;i++)
         A[i]=rand();
-
 }
---
+++ re1_ch11_pointer_Training/re1_pointer_3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h> //rand함수를 위해
+#include <time.h> //매번 rand함수를 다르게 하는 헤더파일
+
+//arr배열의 첫 주소값을 전달받은 배열 A에 rand값을 넣는 함수
+void array_fill(int* A, int size);
+
+int main(void)
+{
+    int arr[10]={0};
+    array_fill(arr,10); 
+    return 0;
+}
+
+void array_fill(int* A, int size)
+{
+    for(int i=0; i<size ; i++)
+    {
+        A[i]=rand()%100; //0~99까지 생성되는 난수가 배열A[i]에 저장됨
+        printf("%d ", A[i]); //두 가지 방법으로 됨  *(A+i)
+    }
+    
+}
+/*
+피드백
+1. 원래 풀었던 문제보다 조금 더 짧게 썼다.
+함수에서 메인함수로 보내지 않고 함수의 for문에 바로 썼다. 
+2. A[i]말고 *(A+i)로 풀 수도 있다. -> 다른 포인터에 대한 이해가 깊어진 것 같다. 
+*/  
